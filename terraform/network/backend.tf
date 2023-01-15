79d186ceaa1cea0bcf6a3fc8aa5cc2dfd84b87db
shlee3048 <shlee3048@naver.com>
2023-01-15 12:21:51 +0000

    first commit

---
+++ terraform/network/backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "s3" {
+#     bucket    = "shlee-terraform-remote-state"
+#     key       = "app.tfstate"
+#     region    = "ap-northeast-2"
+#     profile   = "shlee"
+#   }
+# }
